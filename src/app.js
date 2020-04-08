48579d6c1a48b9ddc551e343bf59b5dc9867d1a7
Alexandre Zelante <alexandre.zelante@outlook.com>
2020-04-08 18:03:38 -0300

    all tests passed

--- src/app.js
+++ src/app.js
@@ -1,33 +1,80 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+let repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get('/repositories', (request, response) => {
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const newRepository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(newRepository);
+
+  return response.json(newRepository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0)
+    return response.status(400).json({ error: 'Repositório não encontrado' });
+
+  if (url) repositories[repositoryIndex].url = url;
+  if (title) repositories[repositoryIndex].title = title;
+  if (techs) repositories[repositoryIndex].techs = techs;
+
+  return response.json(repositories[repositoryIndex]);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  let filteredRepositories = repositories.filter(
+    (repository) => id !== repository.id
+  );
+
+  if (filteredRepositories.length === repositories.length) {
+    return response.status(400).json({ error: 'Repositório não encontrado' });
+  }
+
+  repositories = filteredRepositories;
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find((repository) => repository.id === id);
+
+  if (!repository)
+    return response.status(400).json({ error: 'Repositório não encontrado' });
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
